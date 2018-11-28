88c1a346913ee1f6e18b6dba1e19f4d91b0e2159
Jason Fong <jfong27@calpoly.edu>
2018-11-28 15:08:27 -0800

    Other structs

--- minls.h
+++ minls.h
@@ -1,5 +1,6 @@
 
 
+#define DIRECT_ZONES 7
 
 typedef struct partition_table {
    uint8_t bootind;
@@ -14,3 +15,36 @@ typedef struct partition_table {
    uint32_t size;
 } p_table;
    
+struct super_block {
+   uint32_t ninodes;
+   uint16_t pad1;
+   int16_t i_blocks;
+   int16_t z_blocks;
+   uint16_t firstdata;
+   int16_t log_zone_size;
+   int16_t pad2;
+   uint32_t max_file;
+   uint32_t zones;
+   int16_t magic;
+   int16_t pad3;
+   uint16_t blocksize;
+   uint8_t subversion;
+}
+
+struct inode {
+   uint16_t mode;
+   uint16_t links;
+   uint16_t uid;
+   uint16_t gid;
+   uint32_t size;
+   int32_t atime;
+   int32_t mtime;
+   int32_t ctime;
+   uint32_t zone[DIRECT_ZONES];
+   uint32_t indirect;
+   uint32_t two_indirect;
+   uint32_t unused;
+}
+
+   
+
