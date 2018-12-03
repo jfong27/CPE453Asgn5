c1a2d5394bc8a39e188091c1ad5491d1c8bdfb8a
calvin <calvin@unix2.csc.calpoly.edu>
2018-12-02 21:08:53 -0800

    Fixed some mallocing issue for both the superblock stuff and the inode array.

--- minls.c
+++ minls.c
@@ -6,7 +6,7 @@
 void parse_args(struct arguments *args, int argc, char *argv[]);
 void find_partition_table(FILE *image, struct arguments *args, int type);
 void find_super_block(FILE *image, struct arguments *args);
-void get_inodes(FILE *image, struct arguments *args, inode **inodes);
+void get_inodes(FILE *image, struct arguments *args, inode *inodes);
 
 int zoneSize;
 
@@ -18,13 +18,16 @@ int main(int argc, char *argv[]) {
    }
 
    /*Command line options*/
-   struct arguments *args = malloc(sizeof(args));
+   args *args = malloc(sizeof(struct arguments));
+   printf("%p\n", args);
    args->location = 0;
    args->v = FALSE;
    args->p = FALSE;
    args->s = FALSE;
    args->image = NULL;
-   args->path = NULL;
+   args->path = "/";
+   args->superblock = NULL;
+
 
    parse_args(args, argc, argv);
 
@@ -44,18 +47,21 @@ int main(int argc, char *argv[]) {
    
    find_super_block(image_fp, args);
 
-
-   inode **inodes = malloc(sizeof(inode) * args->superblock->ninodes);
+   inode *inodes = malloc(args->superblock->ninodes * sizeof *inodes);
    get_inodes(image_fp, args, inodes);
 
    fclose(image_fp);
+
+   free(inodes);
+   free(args->superblock);
+   free(args);
 
    return 0;
 }
 
 //TODO: Parse command linei arguments for partition 
 //and verbose flags, and image file name.
-void parse_args(struct arguments *args, int argc, char *argv[]) {
+void parse_args(args *args, int argc, char *argv[]) {
    int i;
 
    for (i = 1; i < argc ; i++) {
@@ -107,10 +113,11 @@ void printPartTable(p_table *ptable) {
 
 //TODO: Seek to partition table in MBR(0x1be), then
 //read it into the p_table struct
-void find_partition_table(FILE *image, struct arguments *args, int type) {
+void find_partition_table(FILE *image, args *args, int type) {
    uint8_t bootSector[BOOT_SECTOR_SIZE];
    p_table *ptable;
 
+   printf("A: %d\n", args->location);
    fseek(image, args->location, SEEK_SET);
    fread(bootSector, BOOT_SECTOR_SIZE, 1, image);
 
@@ -129,7 +136,6 @@ void find_partition_table(FILE *image, struct arguments *args, int type) {
          printf("\nSubpartition table:\n");
 
    }
-
    ptable = (p_table *)(bootSector + PTABLE_LOC);
 
    printPartTable(ptable);
@@ -150,6 +156,7 @@ void find_partition_table(FILE *image, struct arguments *args, int type) {
       exit(3);
    }
    args->location = ptable->lFirst*SECTOR_SIZE;
+   printf("A: %d\n", args->location);
 }
 
 void printSBlock(s_block *superblock) {
@@ -159,8 +166,7 @@ void printSBlock(s_block *superblock) {
    printf("  i_blocks %11d\n", superblock->i_blocks);
    printf("  z_blocks %11d\n", superblock->z_blocks);
    printf("  firstdata %10u\n", superblock->firstdata);
-   printf("  log_zone_size %6d", superblock->log_zone_size);
-   printf(" (zone size: %u)\n", zoneSize);
+   printf("  log_zone_size %6d (zone size: %u)\n", superblock->log_zone_size, zoneSize);
    printf("  max_file %11u\n", superblock->max_file);
    printf("  magic         0x%04x\n", superblock->magic);
    printf("  zones %14u\n", superblock->zones);
@@ -170,29 +176,29 @@ void printSBlock(s_block *superblock) {
 
 void find_super_block(FILE *image, struct arguments *args) {
    uint8_t superBlockSector[SUPER_BLOCK_SIZE];
-   s_block *superblock;
 
    printf("A: %d\n", args->location);
    printf("B: %d\n", args->location+SUPER_BLOCK_SIZE);
    fseek(image, args->location+SUPER_BLOCK_SIZE, SEEK_SET);
    fread(superBlockSector, SUPER_BLOCK_SIZE, 1, image);
 
-   superblock = (s_block *)superBlockSector;
+   args->location += SUPER_BLOCK_SIZE;
 
-   if(superblock->magic != MINIX_MAGIC) {
-      printf("Bad magic number. (0x%04x)\n", superblock->magic);
+   args->superblock = malloc(sizeof(s_block));
+   memcpy(args->superblock, superBlockSector, sizeof(s_block));
+
+   if(args->superblock->magic != MINIX_MAGIC) {
+      printf("Bad magic number. (0x%04x)\n", args->superblock->magic);
       printf("This doesn't look like a MINIX filesystem.\n");
       fclose(image);
       exit(255);
    }
 
-   zoneSize = superblock->blocksize << superblock->log_zone_size;
+   zoneSize = args->superblock->blocksize << args->superblock->log_zone_size;
 
    if(args->v) {
-      printSBlock(superblock);
+      printSBlock(args->superblock);
    }
-
-   args->superblock = superblock;
 }
 
 //TODO: Find partition/subpartition
@@ -208,7 +214,7 @@ void find_filesystem() {
 
 }
 
-void get_inodes(FILE *image, args *args, inode **inodes) {
+void get_inodes(FILE *image, args *args, inode *inodes) {
    printf("C: %d\n", args->location);
 
    uint16_t blocksize = args->superblock->blocksize;
@@ -216,20 +222,17 @@ void get_inodes(FILE *image, args *args, inode **inodes) {
    int16_t z_blocks = args->superblock->z_blocks;
    uint32_t ninodes = args->superblock->ninodes;
 
-   inode **inode_array = malloc(sizeof(inode) * ninodes);
-
    fseek(image,
-         args->location + BOOT_SECTOR_SIZE + SUPER_BLOCK_SIZE + 
+         args->location + BOOT_SECTOR_SIZE + SUPER_BLOCK_SIZE +
          (i_blocks * blocksize) + (z_blocks * blocksize),
          SEEK_SET);
 
    fread(inodes,  sizeof(inode), ninodes, image);
 
-
    fprintf(stderr, "ASDSDA\n");
-   printf("%32u \n", (*inode_array)->uid);
-   printf("%32u \n", inode_array[1]->uid);
-   printf("%32u \n", inode_array[2]->uid);
+   printf("%32u \n", inodes[0].uid);
+   printf("%32u \n", inodes[1].uid);
+   printf("%32u \n", inodes[2].uid);
 }
 
 
